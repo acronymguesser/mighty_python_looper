5bb9e91eccd4d2922d644f7a7bcbabd2a3346d11
Acronym Guesser <acronymguesser@users.noreply.github.com>
2020-04-09 23:47:37 -0300

    added command line option to specify configuration module.

--- app.py
+++ app.py
@@ -1,32 +1,52 @@
+import sys, getopt
 import wave
 import time
 import looper
 
-looper_config = __import__("looper_config")
+def main(argv):
 
-looper.initialize(looper_config.get_looper_settings())
-looper.set_playback_definition_list(looper_config.get_playback_definition_list())
+  looper_config_path = "looper_config"
 
-looper.start_stream()
+  try:
+    opts, *_ = getopt.getopt(argv, "" , ["config="])
+  except getopt.GetoptError as err:
+    print(err)
+    exit()
 
-input()
-looper.exit_looper = True
+  for o, a in opts:
+    if o == "--config":
+      looper_config_path = a
+      if looper_config_path.endswith(".py"):
+        looper_config_path = looper_config_path[:-3]
 
-while looper.is_active():
-  time.sleep(0.1)
+  looper_config = __import__(looper_config_path)
 
-looper.stop_stream()
+  looper.initialize(looper_config.get_looper_settings())
+  looper.set_playback_definition_list(looper_config.get_playback_definition_list())
 
-wrecord = wave.open("record.wav", 'wb')
-wrecord.setnchannels(looper.CHANNELS)
-wrecord.setsampwidth(looper.SAMPLE_SIZE)
-wrecord.setframerate(looper.RATE)
-wrecord.writeframes(looper.recording_wave_data)
-wrecord.close()
+  looper.start_stream()
 
-wplayback = wave.open("playback.wav", 'wb')
-wplayback.setnchannels(looper.CHANNELS)
-wplayback.setsampwidth(looper.SAMPLE_SIZE)
-wplayback.setframerate(looper.RATE)
-wplayback.writeframes(looper.playback_wave_data)
-wplayback.close()
+  input()
+  looper.exit_looper = True
+
+  while looper.is_active():
+    time.sleep(0.1)
+
+  looper.stop_stream()
+
+  wrecord = wave.open("record.wav", 'wb')
+  wrecord.setnchannels(looper.CHANNELS)
+  wrecord.setsampwidth(looper.SAMPLE_SIZE)
+  wrecord.setframerate(looper.RATE)
+  wrecord.writeframes(looper.recording_wave_data)
+  wrecord.close()
+
+  wplayback = wave.open("playback.wav", 'wb')
+  wplayback.setnchannels(looper.CHANNELS)
+  wplayback.setsampwidth(looper.SAMPLE_SIZE)
+  wplayback.setframerate(looper.RATE)
+  wplayback.writeframes(looper.playback_wave_data)
+  wplayback.close()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
